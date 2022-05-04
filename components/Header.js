ab1cc2f0420e42499d6aa9c306667337b4ceff5d
Jorge <gkitas10@gmail.com>
2022-05-04 17:12:23 -0500

    Add links to Header
    Adds links and classes from Header.module.css

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
+
 const Header = () => {
     return ( 
-        <div>
-            Header
+        <div className={styles.header}>
+            <navbar>
+                <Link href='/about'><a className={styles.header__anchor}>Acerca de mi</a></Link>
+                <Link href='/contact'><a className={styles.header__anchor}>Contacto</a></Link>
+            </navbar>
         </div>
      );
 }
