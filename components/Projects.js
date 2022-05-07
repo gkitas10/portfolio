36412fc91e3f083ef391cd6592688898ba83fdc5
Jorge <gkitas10@gmail.com>
2022-05-07 12:58:58 -0500

    Add projects screenshots

--- components/Projects.js
+++ components/Projects.js
@@ -3,13 +3,13 @@ import styles from '../styles/Projects.module.css';
 const Projects = () => {
     return ( 
         <div>
-            <h2 className={styles.title}>Proyectos</h2>
+            <h2 className={styles.title}>{'<Proyectos/>'}</h2>
             <div className={styles.container}>
                 <div className={styles['img-cont']}>
-                    <img src='/accounting.png'/>
+                    <img src='/habitshome.png'/>
                 </div>
                 <div className={styles['img-cont']}>
-                    <img src='/daily-tasks.6e96b94a.png'/>
+                    <img src='/contadorapp.png'/>
                 </div>
             </div>
         </div>
