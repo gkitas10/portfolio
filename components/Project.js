c3c6b95b1560418d1d28e260d1d1579f25c120a3
Jorge <gkitas10@gmail.com>
2022-05-15 10:16:38 -0500

    Add hover effect to projects
    Creates onMouseenter event and handler
    Adds a class to modal to show or hide  depending on project state

--- components/Project.js
+++ components/Project.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import styles from '../styles/Project.module.css';
+import Modal from './Modal';
 
 const Project = ({ src }) => {
+    const [ showclass, setShowclass ] =useState(false);
+
+    const handleMouseEnter = () => {
+        setShowclass(true)
+    }
+
+    const handleMouseLeave = () => {
+        setShowclass(false)
+    }
+
     return ( 
-        <div className={styles['project']}>
+        <div className={styles['project']} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <img src={src}/>
+            <Modal
+            showclass={showclass}
+            />
         </div>
      );
 }
