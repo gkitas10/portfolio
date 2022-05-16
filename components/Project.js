8ab5fd68cf3a359d2a1d591c602146716c5a04fe
Jorge <gkitas10@gmail.com>
2022-05-16 18:38:24 -0500

    Add repos url to projects dinamically
    Each projects renders icons based on info from DB
    Each component shows a link to gh repos (info comes from DB)

--- components/Project.js
+++ components/Project.js
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import styles from '../styles/Project.module.css';
+import ModalStyles from '../styles/Modal.module.css';
 import Modal from './Modal';
+import { AngularIcon, Ghicon, NodejsIcon, ReactIcon, MongodbIcon } from './icons/icons';
+import { iconSwitch } from '../helpers/helpers.js';
 
-const Project = ({ src }) => {
+const Project = ({ src, url, repo, stack }) => {
     const [ showclass, setShowclass ] =useState(false);
 
     const handleMouseEnter = () => {
@@ -14,12 +17,33 @@ const Project = ({ src }) => {
     }
 
     return ( 
-        <div className={styles['project']} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <img src={src}/>
-            <Modal
-            showclass={showclass}
-            />
+        <div className={styles.main}>
+            <div className={styles['project']} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <img src={src}/>
+                <Modal>
+                    <div className={showclass ? ModalStyles.main : ModalStyles['main-hide'] }></div>
+                    <a className={showclass ? ModalStyles.btn : ModalStyles['main-hide']} href={url} target="_blank">Visita el sitio</a>
+                </Modal>
+            </div>
+            <div className={styles.info}>
+                <div className={styles['repo-cont']}>
+                    <div className={styles['repo-row']}>
+                        <Ghicon/>
+                        <div className={styles['links-cont']}>
+                            <a href={repo}>Frontend</a>
+                            <a href={repo}>Backend</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="stack-cont">
+                    { stack.map((tech, idx, arr) => {
+                        
+                        return iconSwitch(tech)
+                    })}
+                </div>
+            </div>
         </div>
+        
      );
 }
  
