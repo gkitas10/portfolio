9bd636db82c68b9d5c374ee4ecd169847eaecf99
Jorge <gkitas10@gmail.com>
2022-05-16 18:35:31 -0500

    Use composition with Modal component
    Renders children in Modal component
    Moves Modal component content

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,9 @@
 import styles from '../styles/Modal.module.css';
 
-const Modal = ({ showclass }) => {
+const Modal = ({ children }) => {
     return (
         <>
-            <div className={showclass ? styles.main : styles['main-hide'] }></div>
-            <a className={showclass ? styles.btn : styles['main-hide']}>Visita el sitio</a>
+            { children }
         </> 
     );
 }
