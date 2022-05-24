2b15d4f1644a032bf36a2ee46482a3025fa97ca5
unknown <gkitas10@gmail.com>
2022-05-24 16:07:50 -0500

    Create cont for logo
    Adds jg logo to logo container next to header navbar

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 import styles from '../styles/Header.module.css';
+import { Jglogo } from './icons/icons';
 
 const Header = () => {
     return ( 
         <div className={styles.header}>
-            <navbar>
-                <Link href='/about'><a className={styles.header__anchor}>Acerca de mi</a></Link>
-                <Link href='/contact'><a className={styles.header__anchor}>Contacto</a></Link>
-            </navbar>
+            <div className={styles['logo-cont']}>
+                <img src='jglogo.svg'/>
+            </div>
+            <div className={styles['nav-cont']}>
+                <navbar>
+                    <Link href='/about-projects'><a className={styles.header__anchor}>Acerca de mis proyectos</a></Link> 
+                    <Link href='/about'><a className={styles.header__anchor}>Acerca de mi</a></Link>
+                    <Link href='/contact'><a className={styles.header__anchor}>Contacto</a></Link>
+                </navbar>
+            </div>
+            
         </div>
      );
 }
