b2ac77943f7294d3ff707a3e6359e6b9e20dd3a7
unknown <gkitas10@gmail.com>
2022-05-24 17:23:27 -0500

    Add googlefonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-            rel="stylesheet"
-          />
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,600&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
@@ -20,3 +22,4 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+
