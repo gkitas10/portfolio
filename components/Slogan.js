c353f12cbb7302058e3f9c3f45c6e8ddda04269d
unknown <gkitas10@gmail.com>
2022-05-24 15:48:46 -0500

    Create slogan component

---
+++ components/Slogan.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Slogan.module.css';
+
+const Slogan = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.content}>
+                Dejame ayudarte a realizar el proyecto de tus sueños
+            </div>
+        </div>
+    );
+}
+ 
+export default Slogan;
