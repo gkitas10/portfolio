08d6080752edcc333fc3780af556b2062577479c
unknown <gkitas10@gmail.com>
2022-05-24 16:11:27 -0500

    Add Slogan to home

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Header from '../components/header'
 import HomeBanner from '../components/HomeBanner'
 import Projects from '../components/Projects'
 import axiosClient from '../axios/axios';
+import Slogan from '../components/Slogan';
 
 export default function Home({ projectsDB }) {
   return (
     <div className={styles.main}>
       <Header/>
       <HomeBanner/>
+      <Slogan/>
       <Projects
       projectsDB={projectsDB}
       />
