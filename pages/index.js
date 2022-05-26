74e691e0d665d87f5bcbda9c0f0930491b15cdd0
unknown <gkitas10@gmail.com>
2022-05-26 17:48:40 -0500

    Wrap Home component content with Layout

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,24 @@ import HomeBanner from '../components/HomeBanner'
 import Projects from '../components/Projects'
 import axiosClient from '../axios/axios';
 import Slogan from '../components/Slogan';
+import Footer from '../components/Footer';
+import DropdownMenu from '../components/DropdownMenu';
+import Layout from '../components/Layout';
 
 export default function Home({ projectsDB }) {
+  console.log(projectsDB);
   return (
     <div className={styles.main}>
-      <Header/>
-      <HomeBanner/>
-      <Slogan/>
-      <Projects
-      projectsDB={projectsDB}
-      />
+      <Layout>
+        <HomeBanner/>
+        <Slogan/>
+        <Projects
+        projectsDB={projectsDB}
+        />
+        <Footer
+        projectsDB={projectsDB}
+        />
+      </Layout>
     </div>
   )
 }
