a7cb9b02321a397ea1c2fd2070330fea573c72ed
unknown <gkitas10@gmail.com>
2022-05-26 17:43:01 -0500

    Get staticProps(projectsDB) on AboutProjectsPage
    Wraps content with Layout
    Renders only one Slider comp for each project

--- pages/about-projects/index.js
+++ pages/about-projects/index.js
@@ -1,29 +1,38 @@
 import styles from "../../styles/AboutProjectsPage.module.css";
-import projectsInfo from "../../components/projects-info/ProjectsInfo";
 import Slider from "../../components/Slider";
+import axiosClient from "../../axios/axios";
+import Layout from "../../components/Layout";
 
-const AboutProjectsPage = () => {
+const AboutProjectsPage = ({ projectsDB }) => {
+  console.log(projectsDB);
   return (
-    <div className={styles.main}>
-      {projectsInfo.map((project, idx) => (
-        <div className={styles.project}>
-          <div className={styles.info}>
-            <h2>{project.name}</h2>
-            <p className={styles.paragraph}>{project.description}</p>
-            <a href={project.tutorialurl} target="__blank">
-              Ir al tutorial
-            </a>
+    <Layout>
+      <div className={styles.main}>
+        {projectsDB.map((project, idx) => (
+          <div className={styles.project}>
+            <div className={styles.info}>
+              <h2>{project.name}</h2>
+              <p className={styles.paragraph}>{project.description}</p>
+            </div>
+            <div className={styles["imgs-section"]}>
+              <Slider project={projectsDB[idx]} />
+            </div>
           </div>
-          <div className={styles["imgs-section"]}>
-            {/* {projectsInfo.map((project, idx) => (
-              <Slider key={idx} project={projectsInfo[idx]}/>
-            ))} */}
-            <Slider project={projectsInfo[idx]}/>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
 export default AboutProjectsPage;
+
+export async function getStaticProps() {
+  const response = await axiosClient.get("/api/projects");
+  const projectsDB = response.data.response;
+
+  return {
+    props: {
+      projectsDB,
+    },
+  };
+}
