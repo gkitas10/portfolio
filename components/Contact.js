e62c6cde329d78ed0ee0bb370c43f8afbe1f7b5d
unknown <gkitas10@gmail.com>
2022-05-27 18:50:05 -0500

    Create Contact component
    Creates Contact component form with submit and change handlers
    Makes post req to api with email data
    Creates state for mail data(there is a bug, there is an undefined property that appears on the state)

---
+++ components/Contact.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import styles from '../styles/Contact.module.css';
+import axiosClient from '../axios/axios';
+
+const Contact = () => {
+    const [ email, setEmail ] = useState({
+        name:'',
+        useremailaddress:'',
+        message:''
+    })
+
+    const handleChange = e => {
+        setEmail({
+            ...email,
+            [e.target.name]:e.target.value,
+            [e.target.useremailaddress]:e.target.value,
+            [e.target.message]:e.target.value
+        })
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        await axiosClient.post('/api/contact', email);
+    }
+    return (
+        <div className={styles.main}>
+            <div className={styles.form_cont}>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <div className={styles.form_group}>
+                        <div className={styles.label}>nombre</div>
+                        <input type="text" name='name' className={styles.form_control} onChange={handleChange}/>
+                    </div>
+                    <div className={styles.form_group}>
+                        <div className={styles.label}>Correo</div>
+                        <input type="text" name='useremailaddress' className={styles.form_control} onChange={handleChange}/>
+                    </div>
+                    <div className={styles.form_group}>
+                        <label className={styles.label}>Mensaje</label>
+                        <textarea className={styles.textarea} name='message' onChange={handleChange}/>
+                    </div>
+                    <div className={styles.form_group}>
+                        <input type='submit' value='Enviar'/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+ 
+export default Contact;
