d4b328e3efc6eb7691ea732e284005dac4aeb7db
unknown <gkitas10@gmail.com>
2022-05-28 15:11:38 -0500

    Fix nodemailer mail undefined bug in api/contact

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,24 +1,29 @@
-export default function (req, res) {
-    console.log(req.body);
-    let nodemailer = require('nodemailer')
+export default async function (req, res) {
+  require('dotenv').config()
+  
+  let nodemailer = require('nodemailer')
 
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-          user: 'demo email',
-          pass: process.env.password,
-        },
-        secure: true,
-      })
+  const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {
+        user: 'portfoliomail10@gmail.com',
+        pass: process.env.password,
+      },
+      secure: true,
+    })
 
-      const mailData = {
-        from: 'demo email',
-        to: 'gkitas10@gmail.com',
-        subject: `Message From ${req.body.name}`,
-        text: req.body.message + " | Sent from: " + req.body.email,
-        html: `<div>${req.body.message}</div><p>Sent from:
-        ${req.body.email}</p>`
-      }
+    console.log(req.body.useremailaddress);
+    const mailData = {
+      from: 'portfoliomail10@gmail.com',
+      to: 'gkitas10@gmail.com',
+      subject: `Message From ${req.body.name}`,
+      text: req.body.message + " | Sent from: " + req.body.useremailaddress,
+      html: `<div>${req.body.message}</div><p>Sent from:${req.body.useremailaddress}</p>`
+    }
     
+    await transporter.sendMail(mailData)
+  
+    res.status(200)  
 }
+
