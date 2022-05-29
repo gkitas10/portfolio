41519fe70d4ca1b2916edc0a50fd64421875dcd9
unknown <gkitas10@gmail.com>
2022-05-29 11:20:25 -0500

    Add Auth0Icon in helpers/helpers.js  and pass smallicon prop to HomePage icons

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,26 +1,29 @@
-import { AngularIcon, NodejsIcon, ReactIcon, MongodbIcon, FirebaseIcon, ExpressIcon } from '../components/icons/icons';
+import { AngularIcon, NodejsIcon, ReactIcon, MongodbIcon, FirebaseIcon, ExpressIcon, Auth0Icon } from '../components/icons/icons';
 
 export const  iconSwitch = (tech, idx) => {
     switch (tech) {
         case 'react':
-            return <ReactIcon key={idx}/>
+            return <ReactIcon key={idx} smallicon/>
             break;
 
         case 'angular':
-            return <AngularIcon key={idx}/>
+            return <AngularIcon key={idx} smallicon/>
             break;
                 
         case 'nodejs':
-            return <NodejsIcon key={idx}/>
+            return <NodejsIcon key={idx} smallicon/>
 
         case 'mongodb':
-            return <MongodbIcon key={idx}/> 
+            return <MongodbIcon key={idx} smallicon/> 
             
         case 'firebase':
-            return <FirebaseIcon key={idx}/>
+            return <FirebaseIcon key={idx} smallicon/>
             
         case 'express':
-            return <ExpressIcon key={idx}/>
+            return <ExpressIcon key={idx} smallicon/>
+
+        case 'auth0':
+            return <Auth0Icon key={idx} smallicon/>
             
         default:
             break;
