a7716ba2b36cede97c189a870fd24eb679689759
unknown <gkitas10@gmail.com>
2022-05-29 14:24:52 -0500

    Create Contact
    Creates email state
    Wrapps Contact with Layout
    Wrapps .form_control's with .input_wrapper
    Fix inputs width so they are aligned

--- components/Contact.js
+++ components/Contact.js
@@ -1,52 +1,80 @@
-import { useState } from 'react';
-import styles from '../styles/Contact.module.css';
-import axiosClient from '../axios/axios';
+import { useState } from "react";
+import styles from "../styles/Contact.module.css";
+import axiosClient from "../axios/axios";
+import Layout from "./Layout";
 
 const Contact = () => {
-    const [ email, setEmail ] = useState({
-        name:'',
-        useremailaddress:'',
-        message:''
-    })
+  const [email, setEmail] = useState({
+    name: "",
+    useremailaddress: "",
+    message: "",
+  });
 
-    const handleChange = e => {
-        setEmail({
-            ...email,
-            [e.target.name]:e.target.value,
-            [e.target.useremailaddress]:e.target.value,
-            [e.target.message]:e.target.value
-        })
-    }
+  const handleChange = (e) => {
+    setEmail({
+      ...email,
+      [e.target.name]: e.target.value,
+      [e.target.useremailaddress]: e.target.value,
+      [e.target.message]: e.target.value,
+    });
+  };
 
-    const handleSubmit = async e => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        await axiosClient.post('/api/contact', email);
-    }
+    await axiosClient.post("/api/contact", email);
+  };
 
-    return (
-        <div className={styles.main}>
-            <div className={styles.form_cont}>
-                <form className={styles.form} onSubmit={handleSubmit}>
-                    <div className={styles.form_group}>
-                        <div className={styles.label}>nombre</div>
-                        <input type="text" name='name' value={email.name} className={styles.form_control} onChange={handleChange}/>
-                    </div>
-                    <div className={styles.form_group}>
-                        <div className={styles.label}>Correo</div>
-                        <input type="email" name='useremailaddress' value={email.useremailaddress} className={styles.form_control} onChange={handleChange}/>
-                    </div>
-                    <div className={styles.form_group}>
-                        <label className={styles.label}>Mensaje</label>
-                        <textarea className={styles.textarea} name='message' value={email.message} onChange={handleChange}/>
-                    </div>
-                    <div className={styles.form_group}>
-                        <input type='submit' value='Enviar'/>
-                    </div>
-                </form>
+  return (
+    <Layout>
+      <div className={styles.main}>
+        <div className={styles.form_cont}>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <div className={styles.form_group}>
+              <div className={styles.label}>Nombre</div>
+              <div className={styles.input_wrapper}>
+                <input
+                    type="text"
+                    name="name"
+                    value={email.name}
+                    className={styles.form_control}
+                    onChange={handleChange}
+                />
+              </div>
+              
             </div>
+            <div className={styles.form_group}>
+              <div className={styles.label}>Correo</div>
+              <div className={styles.input_wrapper}>
+                <input
+                    type="email"
+                    name="useremailaddress"
+                    value={email.useremailaddress}
+                    className={styles.form_control}
+                    onChange={handleChange}
+                />
+              </div>
+              
+            </div>
+            <div className={styles.form_group + ' ' + styles.msg_form_group }>
+              <label className={styles.label}>Mensaje</label>
+              <div className={styles.input_wrapper}>
+                <textarea
+                    className={styles.textarea}
+                    name="message"
+                    value={email.message}
+                    onChange={handleChange}
+                />
+            </div>
+            </div>
+            <div className={styles.form_group}>
+              <input type="submit" value="Enviar" className={styles.input_submit}/>
+            </div>
+          </form>
         </div>
-    );
-}
- 
+      </div>
+    </Layout>
+  );
+};
+
 export default Contact;
