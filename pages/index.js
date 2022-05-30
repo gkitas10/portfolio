10cd61648c54baad387a2b1522ec15b7d89c50e0
unknown <gkitas10@gmail.com>
2022-05-29 19:18:55 -0500

    Eliminate unused components in Home

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import styles from '../styles/Home.module.css';
-import Header from '../components/header'
-import HomeBanner from '../components/HomeBanner'
-import Projects from '../components/Projects'
+import HomeBanner from '../components/HomeBanner';
+import Projects from '../components/Projects';
 import axiosClient from '../axios/axios';
 import Slogan from '../components/Slogan';
 import Footer from '../components/Footer';
-import DropdownMenu from '../components/DropdownMenu';
 import Layout from '../components/Layout';
 
 export default function Home({ projectsDB }) {
