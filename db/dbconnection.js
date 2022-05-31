1898bb646aaacceca0ec671086efa43e3287e70a
unknown <gkitas10@gmail.com>
2022-05-31 15:11:18 -0500

    Add db string

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 async function mongodbConnection () {
-    const client = await MongoClient.connect('mongodb://localhost:27017/portfolio')
+    const client = await MongoClient.connect('mongodb+srv://gkitas89:3YF28dO77j3dSzyB@cluster0.r1vbn.mongodb.net/?retryWrites=true&w=majority'/* 'mongodb://localhost:27017/portfolio' */)
     const db = client.db()
     return {
         client,
