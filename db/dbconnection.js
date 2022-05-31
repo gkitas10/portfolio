0aa3d87b14576c83b4cac4e501e2464ab3a93010
unknown <gkitas10@gmail.com>
2022-05-31 16:18:09 -0500

    Test url for vercel

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,8 +1,8 @@
 import { MongoClient } from 'mongodb';
 
 async function mongodbConnection () {
-    const client = await MongoClient.connect('mongodb+srv://gkitas89:3YF28dO77j3dSzyB@cluster0.r1vbn.mongodb.net/?retryWrites=true&w=majority'/* 'mongodb://localhost:27017/portfolio' */)
-    const db = client.db()
+    const client = await MongoClient.connect('mongodb+srv://gkitas89:3YF28dO77j3dSzyB@cluster0.r1vbn.mongodb.net/?retryWrites=true&w=majority')
+    const db = client.db('portfolio')
     return {
         client,
         db
