8352a44c8253947b26c5d83cde06c20f05fc81b2
unknown <gkitas10@gmail.com>
2022-06-03 16:37:58 -0500

    Render Project or ProjectMobile
    Renders one or the other depending on the result of useMediaQuery

--- components/Projects.js
+++ components/Projects.js
@@ -1,24 +1,39 @@
-import styles from '../styles/Projects.module.css';
-import Project from './Project';
+import styles from "../styles/Projects.module.css";
+import Project from "./Project";
+import ProjectMobile from "./ProjectMobile";
+import { useMediaQuery } from "react-responsive";
 
 const Projects = ({ projectsDB }) => {
-    return ( 
-        <div className={styles.main}>
-            <h2 className={styles.title}>{'<Proyectos/>'}</h2>
-            <div className={styles.container}>
-                { projectsDB.map((project) => (
-                    <Project
-                    key={project._id}
-                    src={project.imgsrc}
-                    url={project.url}
-                    repo={project.repo}
-                    stack={project.stack}
-                    />
-                ))}
-            </div>
-        </div>
-     );
-}
- 
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+
+  return (
+    <div className={styles.main}>
+      <h2 className={styles.title}>{"<Proyectos/>"}</h2>
+      <div className={styles.container}>
+        {projectsDB.map((project) =>
+          isDesktop ? (
+            <Project
+              key={project._id}
+              src={project.imgsrc}
+              url={project.url}
+              repo={project.repo}
+              stack={project.stack}
+            />
+          ) : (
+            <ProjectMobile
+              key={project._id}
+              src={project.imgsrc}
+              url={project.url}
+              repo={project.repo}
+              stack={project.stack}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
 export default Projects;
-    
