e4c03e86a48aa212b13ada3082057f26e291ae25
unknown <gkitas10@gmail.com>
2022-06-04 17:47:31 -0500

    Fix hydration problem with useMediaQuery
    Renders both components (Project and ProjectMobile) in Projects and sets display none depending on screen width instead of rendering components conditionally

--- components/Projects.js
+++ components/Projects.js
@@ -1,19 +1,15 @@
 import styles from "../styles/Projects.module.css";
 import Project from "./Project";
 import ProjectMobile from "./ProjectMobile";
-import { useMediaQuery } from "react-responsive";
 
 const Projects = ({ projectsDB }) => {
-  const isDesktop = useMediaQuery({
-    query: "(min-width: 1224px)",
-  });
 
   return (
     <div className={styles.main}>
       <h2 className={styles.title}>{"<Proyectos/>"}</h2>
       <div className={styles.container}>
-        {projectsDB.map((project) =>
-          isDesktop ? (
+        {projectsDB.map((project) => (
+          <>
             <Project
               key={project._id}
               src={project.imgsrc}
@@ -21,7 +17,6 @@ const Projects = ({ projectsDB }) => {
               repo={project.repo}
               stack={project.stack}
             />
-          ) : (
             <ProjectMobile
               key={project._id}
               src={project.imgsrc}
@@ -29,11 +24,29 @@ const Projects = ({ projectsDB }) => {
               repo={project.repo}
               stack={project.stack}
             />
-          )
-        )}
+          </>
+        ))}
       </div>
     </div>
   );
 };
 
 export default Projects;
+
+// isDesktop ? (
+//   <Project
+//     key={project._id}
+//     src={project.imgsrc}
+//     url={project.url}
+//     repo={project.repo}
+//     stack={project.stack}
+//   />
+// ) : (
+//   <ProjectMobile
+//     key={project._id}
+//     src={project.imgsrc}
+//     url={project.url}
+//     repo={project.repo}
+//     stack={project.stack}
+//   />
+// )
