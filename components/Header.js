04fddf30c442bcaaaca0121ba3f5465df92f6f6b
unknown <gkitas10@gmail.com>
2022-06-05 13:05:38 -0500

    Eliminate dark theme and align BurgerIcon
    Removes themeclass variable which changed based on route
    Every Header is goint to have .main class

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,16 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import styles from '../styles/Header.module.css';
-import { useRouter } from 'next/router';
 import { BurguerIcon, JgIcon } from './icons/icons';
 import Navlinks from './Navlinks';
 import NavlinksMobile from './NavlinksMobile';
 
 const Header = () => {
-    const router = useRouter()
-    const themeclass = router.route === '/about-projects' || router.route === '/contact' || router.route === '/aboutme' ? 'dark' : 'main';
     const [ showmobile, setShowmobile ] = useState(false);
-    console.log(themeclass);
     return ( 
         <div className={ styles.main }>
             <div className={styles['logo-cont']}>
-                <Link href='/'><a className={styles.anchor}>{<JgIcon themeclass={themeclass}/>}</a></Link> 
+                <Link href='/'><a className={styles.anchor}>{<JgIcon/>}</a></Link> 
             </div>
             <div className={styles.burger_wrapper}>
                 <div className={styles.burger_background + (showmobile ? (' ' + styles.burger_background_active) : '')}
@@ -23,10 +19,12 @@ const Header = () => {
                      <BurguerIcon/>
                 </div>
             </div>
+            {/* {
+                showmobile ? <navbar className={styles.nav +' '+ styles.nav_mobile}><NavlinksMobile themeclass={themeclass}/></navbar> : <navbar className={styles.nav}><Navlinks themeclass={themeclass}/></navbar>
+            } */}
             <navbar className={styles.nav}>
-                { showmobile ? <NavlinksMobile themeclass={themeclass}/> : <Navlinks themeclass={themeclass}/> }
+                { showmobile ? <NavlinksMobile/> : <Navlinks/> }
             </navbar>
-                {/* <div className={styles['nav-cont']}></div> */}
         </div>
      );
 }
