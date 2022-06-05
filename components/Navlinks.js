33f64235fcf5afab1ad27cfd1e5f5c8a4be7ecdb
unknown <gkitas10@gmail.com>
2022-06-05 13:09:51 -0500

    Eliminate themeclass prop from Navlinks and NavlinksMobile, there are not themes anymore

--- components/Navlinks.js
+++ components/Navlinks.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 import styles from '../styles/Navlinks.module.css';
 
-const Navlinks = (themeclass) => {
+const Navlinks = () => {
     return (
         <ul className={styles.main}>
-            <Link href='/about-projects'><a className={styles.anchor + (themeclass === 'dark' ? (' ' + styles.anchor_dark) : '') }>Acerca de mis proyectos</a></Link> 
-            <Link href='/aboutme'><a className={styles.anchor + (themeclass === 'dark' ? (' ' + styles.anchor_dark) : '') }>Acerca de mi</a></Link>
-            <Link href='/contact'><a className={styles.anchor + (themeclass === 'dark' ? (' ' + styles.anchor_dark) : '') }>Contacto</a></Link>
+            <Link href='/about-projects'><a className={styles.anchor}>Acerca de mis proyectos</a></Link> 
+            <Link href='/aboutme'><a className={styles.anchor}>Acerca de mi</a></Link>
+            <Link href='/contact'><a className={styles.anchor}>Contacto</a></Link>
         </ul>
     );
 }
