1bdc17b5a4966d3a0730859ab7fde40eee57cee5
unknown <gkitas10@gmail.com>
2022-06-25 20:07:12 -0500

    Add lang folder with locales and eliminate trash code in DropdownMenu

---
+++ lang/home.js
@@ -0,0 +1,50 @@
+const home = {
+    en:{
+        header:{navlinks:{
+            aboutme:"About me",
+            aboutprojects:"About projects",
+            contact:"Contact"
+        }},
+        homebanner:{
+            greeting:"Hi",
+            iam:'I am',
+            contactme:"Contact me"
+        },
+        slogan:{
+            content:"Let me help you build the project of your dreams"
+        },
+        projects:{
+            title:"<Projects/>"
+        },
+        footer:{
+            aboutportfolio:"About this portfolio",
+            contact:"Contact",
+            tutorials:"Tutorials"
+        }
+    },
+    es:{
+        header:{navlinks:{
+            aboutme:"Acerca de mi",
+            aboutprojects:"Acerca de mis proyectos",
+            contact:"Contacto"
+        }},
+        homebanner:{
+            greeting:"Hola",
+            iam:'Soy',
+            contactme:"Contáctame"
+        },
+        slogan:{
+            content:"Déjame ayudarte a realizar el proyecto de tus sueños"
+        },
+        projects:{
+            title:"<Proyectos/>"
+        },
+        footer:{
+            aboutportfolio:"Acerca de este portafolio",
+            contact:"Contacto",
+            tutorials:"Tutoriales"
+        }
+    }
+}
+
+export default home;
